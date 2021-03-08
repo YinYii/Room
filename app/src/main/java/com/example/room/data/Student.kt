9f7c6f1bcbd94c58cb23171d812d853ea5bcdd59
YinYii <ohyy-wp18@student.tarc.edu.my>
2021-03-09 00:07:33 +0800

    init

---
+++ app/src/main/java/com/example/room/data/Student.kt
@@ -0,0 +1,14 @@
+package com.example.room.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "student_table")
+// by setting data no need set and get
+data class Student (
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val name:String,
+    val programme:String
+)
